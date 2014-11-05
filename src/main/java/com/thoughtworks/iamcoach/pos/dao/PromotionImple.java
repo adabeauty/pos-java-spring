e1219be812aa4eeddbfdea257999f7d7aec4fc07
adabeauty <654296217@qq.com>
2014-11-06 00:32:12 +0800

    add PromotionRowMapper to comstructor

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionImple.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionImple.java
@@ -8,18 +8,20 @@ import java.util.ArrayList;
 
 public class PromotionImple implements PromotionDao {
     private SimpleJdbcTemplate simpleJdbcTemplate;
+    private PromotionRowMapper promotionRowMapper;
 
-    public PromotionImple(SimpleJdbcTemplate simpleJdbcTemplate) {
+    public PromotionImple(SimpleJdbcTemplate simpleJdbcTemplate, PromotionRowMapper promotionRowMapper) {
         this.simpleJdbcTemplate = simpleJdbcTemplate;
+        this.promotionRowMapper = promotionRowMapper;
     }
 
     public Promotion getPromotionByType(int type) {
         String sql =  "SELECT * FROM promotions WHERE type = ?";
-        return (Promotion)simpleJdbcTemplate.queryForObject(sql, new PromotionRowMapper(), type);
+        return (Promotion)simpleJdbcTemplate.queryForObject(sql, promotionRowMapper, type);
     }
 
     public ArrayList<Promotion> getPromotions() {
         String sql = "SELECT * FROM promotions";
-        return ( ArrayList<Promotion>)simpleJdbcTemplate.query(sql, new PromotionRowMapper());
+        return ( ArrayList<Promotion>)simpleJdbcTemplate.query(sql, promotionRowMapper);
     }
 }
