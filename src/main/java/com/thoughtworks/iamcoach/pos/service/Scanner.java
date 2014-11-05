09e1af3ce8e57614f5a7aded72e04ccf3fafb90b
adabeauty <654296217@qq.com>
2014-11-05 18:58:22 +0800

    add constructor to Scanner

--- src/main/java/com/thoughtworks/iamcoach/pos/service/Scanner.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/Scanner.java
@@ -5,7 +5,12 @@ import com.thoughtworks.iamcoach.pos.util.ReadFileUlti;
 import java.util.ArrayList;
 
 public class Scanner {
+    private String fileName;
+    public Scanner(String fileName) {
+        this.fileName = fileName;
+    }
+
     public ArrayList<String> getInputs(){
-        return ReadFileUlti.readFile("cart.txt");
+        return ReadFileUlti.readFile(fileName);
     }
 }
