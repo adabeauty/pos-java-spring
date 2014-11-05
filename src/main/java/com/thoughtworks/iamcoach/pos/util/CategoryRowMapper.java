d992cf27f7f9c7d4738f8a473924cf9f73230a35
adabeauty <654296217@qq.com>
2014-11-05 20:01:12 +0800

    finish CategoryRowMapper

--- src/main/java/com/thoughtworks/iamcoach/pos/util/CategoryRowMapper.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/util/CategoryRowMapper.java
@@ -1,7 +1,17 @@
 package com.thoughtworks.iamcoach.pos.util;
 
-/**
- * Created by wanghuan on 14-11-5.
- */
-public class CategoryRowMapper {
+import com.thoughtworks.iamcoach.pos.model.Category;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CategoryRowMapper implements RowMapper{
+
+    public Category mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Category category = new Category();
+        category.setName(rs.getString("name"));
+        category.setId(rs.getString("id"));
+        return category;
+    }
 }
