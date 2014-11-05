4958fdcdf596cd4e3a2495f3089db836d2611b3f
adabeauty <654296217@qq.com>
2014-11-05 18:03:54 +0800

    delete useless codes

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/ItemImple.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/ItemImple.java
@@ -15,55 +15,25 @@ public class ItemImple implements ItemDao {
         this.simpleJdbcTemplate = simpleJdbcTemplate;
     }
 
-    private PreparedStatement pstmt = null;
-    private ResultSet rs = null;
-    private ConnctionUlti connctionUlti = new ConnctionUlti();
-
-
     public ArrayList<Item> getItems() {
-g        String sql = "SELECT * FROM items";
+        String sql = "SELECT * FROM items";
         return  (ArrayList<Item>)simpleJdbcTemplate.query(sql, new UserRowMapper());
     }
 
     public ArrayList<Promotion> getPromotions(int id) {
-        ArrayList<Promotion> promotions = new ArrayList<Promotion>();
         String sql = "SELECT promotions.*, relationship.discount FROM promotions, relationship " +
                 "WHERE relationship.itemId=? AND promotions.id=relationship.promotionId";
 
-        Connection connection = connctionUlti.getConnection();
-        try {
-            pstmt = connection.prepareStatement(sql);
-            pstmt.setInt(1,id);
-
-            rs = pstmt.executeQuery();
-            while(rs.next()){
+        return (ArrayList<Promotion>)simpleJdbcTemplate.query(sql, new RowMapper<Promotion>() {
+            public Promotion mapRow(ResultSet rs, int i) throws SQLException {
                 Promotion promotion = PromotionFactory.generatePromotion(rs.getInt("type"));
-
                 promotion.setId(rs.getInt("id"));
-                promotion.setId(rs.getInt("type"));
+                promotion.setType(rs.getInt("type"));
                 promotion.setDescription(rs.getString("description"));
                 promotion.setDiscount(rs.getDouble("discount"));
-
-                promotions.add(promotion);
+                return promotion;
             }
-
-            closeAllConnrction();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return promotions;
-    }
-
-    private void closeAllConnrction(){
-        connctionUlti.closeConnection();
-        try {
-            pstmt.close();
-            rs.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        }, id);
     }
 
     private static final class UserRowMapper implements RowMapper {
