d1ca89488852384ae2dc4d747efaa644d9b55f2e
adabeauty <654296217@qq.com>
2014-11-06 00:05:21 +0800

    add constructor

--- src/main/java/com/thoughtworks/iamcoach/pos/util/CategoryRowMapper.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/util/CategoryRowMapper.java
@@ -7,6 +7,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CategoryRowMapper implements RowMapper{
+    private Category category;
+
+    public CategoryRowMapper(Category category) {
+        this.category = category;
+    }
 
     public Category mapRow(ResultSet rs, int rowNum) throws SQLException {
         Category category = new Category();
