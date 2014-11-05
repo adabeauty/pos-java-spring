093551eec322396c0381098bd8552c632a40baca
adabeauty <654296217@qq.com>
2014-11-05 18:12:37 +0800

    delete useless codes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,16 +1,15 @@
 import com.thoughtworks.iamcoach.pos.dao.ItemDao;
-import com.thoughtworks.iamcoach.pos.dao.ItemImple;
-import com.thoughtworks.iamcoach.pos.service.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
 
-        ItemDao itemImpl = new ItemImple();
-        Scanner scanner = new Scanner();
-        CartService cartService = new CartService(scanner, itemImpl);
 
-        Printer printer = new Printer(cartService);
-        System.out.println(printer.printList());
+        ItemDao itemDaoImpl  = (ItemDao)applicationContext.getBean("itemImpl");
+        System.out.println(itemDaoImpl.getPromotions(1));
+
     }
 }
