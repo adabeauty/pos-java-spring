18c3fe5de01c37763191b3e54cbdaa0b3c06e12b
adabeauty <654296217@qq.com>
2014-11-05 17:32:20 +0800

    add empty constructor and setters

--- src/main/java/com/thoughtworks/iamcoach/pos/model/Category.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/model/Category.java
@@ -4,6 +4,9 @@ public class Category {
     private String id;
     private String name;
 
+    public Category() {
+    }
+
     public Category(String id, String name) {
         this.name = name;
         this.id = id;
@@ -15,6 +18,14 @@ public class Category {
 
     public String getName() {
         return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
