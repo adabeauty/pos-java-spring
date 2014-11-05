adb3128e2a97e77d920ec4fc30a9de8ceb16d505
adabeauty <654296217@qq.com>
2014-11-05 20:10:27 +0800

    finish class

--- src/main/java/com/thoughtworks/iamcoach/pos/util/PromotionRowMapper.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/util/PromotionRowMapper.java
@@ -1,7 +1,19 @@
 package com.thoughtworks.iamcoach.pos.util;
 
-/**
- * Created by wanghuan on 14-11-5.
- */
-public class PromotionRowMapper {
+import com.thoughtworks.iamcoach.pos.model.Promotion;
+import com.thoughtworks.iamcoach.pos.model.PromotionFactory;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PromotionRowMapper implements RowMapper {
+    public Promotion mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Promotion promotion = PromotionFactory.generatePromotion(rs.getInt("type"));
+        promotion.setId(rs.getInt("id"));
+        promotion.setType(rs.getInt("type"));
+        promotion.setDescription(rs.getString("description"));
+        promotion.setDiscount(rs.getDouble("discount"));
+        return promotion;
+    }
 }
