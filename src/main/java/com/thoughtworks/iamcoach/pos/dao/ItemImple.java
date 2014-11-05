7dae0ee39c826542930ffae5e0304ecec814fd96
adabeauty <654296217@qq.com>
2014-11-05 20:15:41 +0800

    delete useless codes

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/ItemImple.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/ItemImple.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
 import com.thoughtworks.iamcoach.pos.model.*;
+import com.thoughtworks.iamcoach.pos.util.ItemRowMapper;
+import com.thoughtworks.iamcoach.pos.util.PromotionRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 
@@ -17,41 +19,12 @@ public class ItemImple implements ItemDao {
 
     public ArrayList<Item> getItems() {
         String sql = "SELECT * FROM items";
-        return  (ArrayList<Item>)simpleJdbcTemplate.query(sql, new UserRowMapper());
+        return  (ArrayList<Item>)simpleJdbcTemplate.query(sql, new ItemRowMapper());
     }
 
     public ArrayList<Promotion> getPromotions(int id) {
         String sql = "SELECT promotions.*, relationship.discount FROM promotions, relationship " +
                 "WHERE relationship.itemId=? AND promotions.id=relationship.promotionId";
-
-        return (ArrayList<Promotion>)simpleJdbcTemplate.query(sql, new RowMapper<Promotion>() {
-            public Promotion mapRow(ResultSet rs, int i) throws SQLException {
-                Promotion promotion = PromotionFactory.generatePromotion(rs.getInt("type"));
-                promotion.setId(rs.getInt("id"));
-                promotion.setType(rs.getInt("type"));
-                promotion.setDescription(rs.getString("description"));
-                promotion.setDiscount(rs.getDouble("discount"));
-                return promotion;
-            }
-        }, id);
-    }
-
-    private static final class UserRowMapper implements RowMapper {
-
-        public Item mapRow(ResultSet rs, int rowNum) {
-            Item item = new Item();
-            try {
-                item.setId(rs.getString("id"));
-                item.setCategoryId(rs.getInt("categoryId"));
-                item.setBarcode(rs.getString("barcode"));
-                item.setName(rs.getString("name"));
-                item.setUnit(rs.getString("unit"));
-                item.setPrice(rs.getDouble("price"));
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            return item;
-        }
+        return  (ArrayList<Promotion>)simpleJdbcTemplate.query(sql, new PromotionRowMapper(), id);
     }
 }
