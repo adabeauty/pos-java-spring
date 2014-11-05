cb65feead7cf9ea7d6497b2951b74ac5c97b5ba2
adabeauty <654296217@qq.com>
2014-11-05 17:59:28 +0800

    delete useless codes

--- src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
@@ -16,15 +16,7 @@ public class ItemService {
         return itemImple.getItems();
     }
 
-//    public Item getItemByBarcode(String barcode) {
-//        return itemImple.getItemByBarcode(barcode);
-//    }
-
     public ArrayList<Promotion> getPromotions(int id) {
         return itemImple.getPromotions(id);
     }
-
-//    public Category getCategory(int id) {
-//        return itemImple.getCategory(id);
-//    }
 }
