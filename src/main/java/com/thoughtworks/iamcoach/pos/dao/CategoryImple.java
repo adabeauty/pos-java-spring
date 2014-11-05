1c76ec003ae29dc03749734e07bc7d88a339bede
adabeauty <654296217@qq.com>
2014-11-05 20:02:08 +0800

    delete useless codes

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/CategoryImple.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/CategoryImple.java
@@ -1,10 +1,8 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
 import com.thoughtworks.iamcoach.pos.model.Category;
-import org.springframework.jdbc.core.RowMapper;
+import com.thoughtworks.iamcoach.pos.util.CategoryRowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
-
-import java.sql.*;
 
 import java.util.ArrayList;
 
@@ -17,20 +15,11 @@ public class CategoryImple implements CategoryDao{
 
     public ArrayList<Category> getCategories() {
         String sql = "SELECT * FROM categories";
-        return (ArrayList<Category>)simpleJdbcTemplate.query(sql, new UserRowMapper());
+        return (ArrayList<Category>)simpleJdbcTemplate.query(sql, new CategoryRowMapper());
     }
 
     public Category getCategoryById(int id) {
         String sql = "SELECT * FROM categories WHERE id = ?";
-        return (Category)simpleJdbcTemplate.queryForObject(sql, new UserRowMapper(), id);
-    }
-
-    private static final class UserRowMapper implements RowMapper {
-        public Category mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Category category = new Category();
-            category.setName(rs.getString("name"));
-            category.setId(rs.getString("id"));
-            return category;
-        }
+        return (Category)simpleJdbcTemplate.queryForObject(sql, new CategoryRowMapper(), id);
     }
 }
