df7f6dc6c109d9203816fa01e816c0b1b1df3567
adabeauty <654296217@qq.com>
2014-11-05 18:13:50 +0800

    finish app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,10 @@
-import com.thoughtworks.iamcoach.pos.dao.ItemDao;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
-
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
-
-
-        ItemDao itemDaoImpl  = (ItemDao)applicationContext.getBean("itemImpl");
-        System.out.println(itemDaoImpl.getPromotions(1));
-
+        Printer printer = (Printer) applicationContext.getBean("printer");
+        System.out.println(printer.printList());
     }
 }
