73c8a670458aae2450cbfeba5ea235e626990ccf
adabeauty <654296217@qq.com>
2014-11-06 16:55:13 +0800

    modify ItemImle to ItemImpl

--- src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/ItemService.java
@@ -1,22 +1,22 @@
 package com.thoughtworks.iamcoach.pos.service;
 
-import com.thoughtworks.iamcoach.pos.dao.ItemImple;
+import com.thoughtworks.iamcoach.pos.dao.ItemImpl;
 import com.thoughtworks.iamcoach.pos.model.*;
 
 import java.util.ArrayList;
 
 public class ItemService {
-    private ItemImple itemImple;
+    private ItemImpl itemImpl;
 
-    public ItemService(ItemImple itemImple) {
-        this.itemImple = itemImple;
+    public ItemService(ItemImpl itemImpl) {
+        this.itemImpl = itemImpl;
     }
 
     public ArrayList<Item> getItems() {
-        return itemImple.getItems();
+        return itemImpl.getItems();
     }
 
     public ArrayList<Promotion> getPromotions(int id) {
-        return itemImple.getPromotions(id);
+        return itemImpl.getPromotions(id);
     }
 }
