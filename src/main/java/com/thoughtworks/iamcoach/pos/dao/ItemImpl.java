5aa79e4151dc6bfea1fb5364667c2fdffb5a0733
adabeauty <654296217@qq.com>
2014-11-08 16:51:45 +0800

    delte ItemRowMapper

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/ItemImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/ItemImpl.java
@@ -3,23 +3,29 @@ package com.thoughtworks.iamcoach.pos.dao;
 import com.thoughtworks.iamcoach.pos.model.*;
 import com.thoughtworks.iamcoach.pos.util.*;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ItemImpl implements ItemDao {
     private JdbcTemplate jdbcTemplate;
-    private ItemRowMapper itemRowMapper;
     private PromotionRowMapper promotionRowMapper;
 
-    public ItemImpl(JdbcTemplate jdbcTemplate, ItemRowMapper itemRowMapper, PromotionRowMapper promotionRowMapper) {
+    public ItemImpl(JdbcTemplate jdbcTemplate, PromotionRowMapper promotionRowMapper) {
         this.jdbcTemplate = jdbcTemplate;
-        this.itemRowMapper = itemRowMapper;
         this.promotionRowMapper = promotionRowMapper;
     }
 
     public ArrayList<Item> getItems() {
         String sql = "SELECT * FROM items";
-        return  (ArrayList<Item>)jdbcTemplate.query(sql, itemRowMapper);
+        return  (ArrayList<Item>)jdbcTemplate.query(sql, new RowMapper<Item>() {
+            public Item mapRow(ResultSet rs, int i) throws SQLException {
+                return new Item(rs.getString("id"), rs.getInt("categoryId"), rs.getString("barcode"),
+                        rs.getString("name"), rs.getString("unit"), rs.getDouble("price"));
+            }
+        });
     }
 
     public ArrayList<Promotion> getPromotions(int id) {
